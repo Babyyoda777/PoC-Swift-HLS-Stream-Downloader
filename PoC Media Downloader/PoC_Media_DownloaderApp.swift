2d394830d5a1305079918495e08f18dd16c61718
Muhammad Shah <80623330+Babyyoda777@users.noreply.github.com>
2024-04-01 10:41:56 +0100

    added files

---
+++ PoC Media Downloader/PoC_Media_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoC_Media_DownloaderApp.swift
+//  PoC Media Downloader
+//
+//  Created by Muhammad Shah on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PoC_Media_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DownloadView()
+        }
+    }
+}
